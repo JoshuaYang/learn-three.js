b1a1de8783fff03971d74a0d25a597912d19a2ca
ヨシュア <yss901025@gmail.com>
2017-02-02 10:13:04 +0800

    u

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 var renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('cv'),
 });
-renderer.setClearColor(0x0000ff);
+// renderer.setClearColor(0x0000ff);
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
+var camera = new THREE.PerspectiveCamera(45, 400 / 300, 1, 10);
 camera.position.set(0, 0, 5);
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 scene.add(camera);
 
 
 
 var cube = new THREE.Mesh(
-    new THREE.CubeGeometry(1, 2, 3),
+    new THREE.CubeGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({
         color: 0xff0000,
+        wireframe: true,
     })
 );
 
