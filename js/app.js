6e7e74f9e64cbd8dc36a86781eb5f93c28b0cca2
ヨシュア <yss901025@gmail.com>
2017-02-03 13:30:49 +0800

    u

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+var controls = {
+    rotationSpeed: 0.02,
+    bouncingSpeed: 0.03,
+}
+
+var gui = new dat.GUI();
+gui.add(controls, 'rotationSpeed', 0, 0.5);
+gui.add(controls, 'bouncingSpeed', 0, 0.5);
+
+
+var stats = initStats();
+
 var ww = window.innerWidth;
 var wh = window.innerHeight;
 
@@ -10,6 +22,7 @@ camera.lookAt(scene.position);
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0xeeeeee);
 renderer.setSize(ww, wh);
+renderer.shadowMapEnabled = true;
 document.getElementById('box').appendChild(renderer.domElement);
 
 var axes = new THREE.AxisHelper(20);
@@ -23,31 +36,76 @@ var planeMaterial = new THREE.MeshLambertMaterial({
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
 plane.position.set(15, 0, 0);
+plane.receiveShadow = true;
 scene.add(plane);
 
 var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
 var cubeMaterial = new THREE.MeshLambertMaterial({
     color: 0xff0000,
-    // wireframe: true,
 });
 
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.position.set(-4, 3, 0);
+cube.castShadow = true;
 scene.add(cube);
 
 var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
 var sphereMaterial = new THREE.MeshLambertMaterial({
     color: 0x7777ff,
-    // wireframe: true,
 });
 
 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.set(20, 4, 2);
+sphere.castShadow = true;
 scene.add(sphere);
 
 var spotLight = new THREE.SpotLight(0xffffff);
 spotLight.position.set(-40, 60, -10);
+spotLight.castShadow = true;
 scene.add(spotLight);
 
 
-renderer.render(scene, camera);
+
+function initStats(){
+    var stats = new Stats();
+
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = 0;
+    stats.domElement.style.top = 0;
+    document.body.appendChild(stats.domElement);
+
+    return stats;
+}
+
+
+var step = 0;
+function renderScene(){
+    stats.update();
+
+    cube.rotation.x += controls.rotationSpeed;
+    cube.rotation.y += controls.rotationSpeed;
+    cube.rotation.z += controls.rotationSpeed;
+
+    step += controls.bouncingSpeed;
+    sphere.position.x = 20 + (10 * Math.cos(step));
+    sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+    requestAnimationFrame(renderScene);
+
+    renderer.render(scene, camera);
+}
+
+function onResize(){
+    var ww = window.innerWidth;
+    var wh = window.innerHeight;
+
+    camera.aspect = ww / wh;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(ww, wh);
+}
+
+window.addEventListener('resize', onResize, false);
+
+renderScene();
