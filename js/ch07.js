f01b130e5cdc598682a1c0842b92a0c209ae02c5
ヨシュア <yss901025@gmail.com>
2017-02-13 09:59:56 +0800

    u

--- js/ch07.js
+++ js/ch07.js
@@ -2,76 +2,29 @@ var stats = initStats();
         // create a scene, that will hold all our elements such as objects, cameras and lights.
         var scene = new THREE.Scene();
         // create a camera, which defines where we're looking at.
-        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 200);
         // create a render and set the size
         var webGLRenderer = new THREE.WebGLRenderer();
-        webGLRenderer.setClearColor(new THREE.Color(0x000000, 1.0));
+        webGLRenderer.setClearColor(0x000000);
         webGLRenderer.setSize(window.innerWidth, window.innerHeight);
         // position and point the camera to the center of the scene
         camera.position.x = 20;
-        camera.position.y = 0;
-        camera.position.z = 150;
+        camera.position.y = 40;
+        camera.position.z = 110;
+        camera.lookAt(new THREE.Vector3(20, 30, 0));
         // add the output of the renderer to the html element
         document.getElementById("WebGL-output").appendChild(webGLRenderer.domElement);
-        var getTexture = function () {
-            var canvas = document.createElement('canvas');
-            canvas.width = 32;
-            canvas.height = 32;
-            var ctx = canvas.getContext('2d');
-            // the body
-            ctx.translate(-81, -84);
-            ctx.fillStyle = "orange";
-            ctx.beginPath();
-            ctx.moveTo(83, 116);
-            ctx.lineTo(83, 102);
-            ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
-            ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
-            ctx.lineTo(111, 116);
-            ctx.lineTo(106.333, 111.333);
-            ctx.lineTo(101.666, 116);
-            ctx.lineTo(97, 111.333);
-            ctx.lineTo(92.333, 116);
-            ctx.lineTo(87.666, 111.333);
-            ctx.lineTo(83, 116);
-            ctx.fill();
-            // the eyes
-            ctx.fillStyle = "white";
-            ctx.beginPath();
-            ctx.moveTo(91, 96);
-            ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
-            ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
-            ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
-            ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
-            ctx.moveTo(103, 96);
-            ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
-            ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
-            ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
-            ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
-            ctx.fill();
-            // the pupils
-            ctx.fillStyle = "blue";
-            ctx.beginPath();
-            ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
-            ctx.fill();
-            ctx.beginPath();
-            ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
-            ctx.fill();
-            var texture = new THREE.Texture(canvas);
-            texture.needsUpdate = true;
-            return texture;
-        };
+        var system1;
         var cloud;
         var controls = new function () {
-            this.size = 15;
+            this.size = 3;
             this.transparent = true;
             this.opacity = 0.6;
             this.color = 0xffffff;
-            this.rotateSystem = true;
             this.sizeAttenuation = true;
             this.redraw = function () {
-                if (scene.getObjectByName("pointcloud")) {
-                    scene.remove(scene.getObjectByName("pointcloud"));
-                }
+                scene.remove(scene.getObjectByName("particles1"));
+                scene.remove(scene.getObjectByName("particles2"));
                 createPointCloud(controls.size, controls.transparent, controls.opacity, controls.sizeAttenuation, controls.color);
             };
         };
@@ -81,37 +34,43 @@ var stats = initStats();
         gui.add(controls, 'opacity', 0, 1).onChange(controls.redraw);
         gui.addColor(controls, 'color').onChange(controls.redraw);
         gui.add(controls, 'sizeAttenuation').onChange(controls.redraw);
-        gui.add(controls, 'rotateSystem');
         controls.redraw();
         render();
         function createPointCloud(size, transparent, opacity, sizeAttenuation, color) {
+            var texture = THREE.ImageUtils.loadTexture("assets/textures/particles/raindrop-3.png");
             var geom = new THREE.Geometry();
-            var material = new THREE.PointsMaterial({
+            var material = new THREE.ParticleBasicMaterial({
                 size: size,
                 transparent: transparent,
                 opacity: opacity,
-                map: getTexture(),
+                map: texture,
+                blending: THREE.AdditiveBlending,
                 sizeAttenuation: sizeAttenuation,
                 color: color
             });
-            var range = 500;
-            for (var i = 0; i < 5000; i++) {
-                var particle = new THREE.Vector3(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2);
+            var range = 40;
+            for (var i = 0; i < 1500; i++) {
+                var particle = new THREE.Vector3(
+                        Math.random() * range - range / 2,
+                        Math.random() * range * 1.5,
+                        Math.random() * range - range / 2);
+                particle.velocityY = 0.1 + Math.random() / 5;
+                particle.velocityX = (Math.random() - 0.5) / 3;
                 geom.vertices.push(particle);
             }
-            cloud = new THREE.Points(geom, material);
-            cloud.name = 'pointcloud';
+            cloud = new THREE.ParticleSystem(geom, material);
             cloud.sortParticles = true;
             scene.add(cloud);
         }
-        var step = 0;
         function render() {
             stats.update();
-            if (controls.rotateSystem) {
-                step += 0.01;
-                cloud.rotation.x = step;
-                cloud.rotation.z = step;
-            }
+            var vertices = cloud.geometry.vertices;
+            vertices.forEach(function (v) {
+                v.y = v.y - (v.velocityY);
+                v.x = v.x - (v.velocityX);
+                if (v.y <= 0) v.y = 60;
+                if (v.x <= -20 || v.x >= 20) v.velocityX = v.velocityX * -1;
+            });
             requestAnimationFrame(render);
             webGLRenderer.render(scene, camera);
         }
